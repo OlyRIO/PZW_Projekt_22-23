71b51b2a2a939c123bcd0bf8ad77993b443e552d
OlyRIO <106329539+OlyRIO@users.noreply.github.com>
2022-12-15 15:09:15 +0100

    Update settings.py

--- PZW_Projekt_22-23-main/skola/skola/settings.py
+++ PZW_Projekt_22-23-main/skola/skola/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j+545pmawsh3p4n)^47=5szn+%p-_!7w!#(6m8s)t)n5284$l!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
