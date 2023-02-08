8a8cbcfcdeff14aa0bb8032d653b7b00ce5aed4a
lukica5 <106252061+lukica5@users.noreply.github.com>
2023-02-08 21:05:21 +0100

    Add files via upload

--- skola/main/forms.py
+++ skola/main/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from main.models import *
 
 
 # Create your forms here.
@@ -18,3 +19,29 @@ class NewUserForm(UserCreationForm):
 		if commit:
 			user.save()
 		return user
+
+
+
+class ProfesorForm(forms.ModelForm):
+    
+    class Meta:
+        model = Profesor
+        fields = ('profesor_ime', 'profesor_prezime', 'profesor_placa')
+        
+class PredmetForm(forms.ModelForm):
+    
+    class Meta:
+        model = Predmet
+        fields = ('predmet_naziv', 'predmet_opis', 'predmet_predavac')
+        
+class UcenikForm(forms.ModelForm):
+    
+    class Meta:
+        model = Ucenik
+        fields = ('ucenik_ime', 'ucenik_prezime', 'ucenik_predmeti', 'ucenik_razrednik')
+
+class UcionicaForm(forms.ModelForm):
+    
+    class Meta:
+        model = Ucionica
+        fields = ('predmet', 'ucionica_broj', 'ucionica_kvadratura')
